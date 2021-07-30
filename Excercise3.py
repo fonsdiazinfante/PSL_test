f55b910f9d4ab2d87d3269c56d47f91596dc4a31
Alfonso Díaz Infante <58762368+fonsdiazinfante@users.noreply.github.com>
2021-07-29 23:22:01 -0500

    Add files via upload

--- Excercise3.py
+++ Excercise3.py
@@ -15,17 +15,13 @@ start = time.time() #save the time when the program started running
 
 usersDict = {} #define an empty Dictionary to store the username and address
 
-files = glob.glob('/home/user/Documents/user_profiles/*', recursive=True) #get all files inside the path stated as the first argumnent
+files = glob.glob('JSON/*', recursive=True) #get all files inside the path stated as the first argumnent
 
 for single_file in files: #loop through all the files
     with open(single_file) as json_file: #open the files
         data = json.load(json_file) #load the json data
         if data['registration_address'][0]['type']=="work": #if the users address is type "work"
-            print(data['user_id'])
-            print(data['registration_address'][0]['address']['address_line_1'])
             usersDict.update({data['user_id']: data['registration_address'][0]['address']['address_line_1']}) #add userid and address to the dictionary
-        else:
-            print("jaja")
 
 with open('users.tsv', 'w') as f: #create a new tsv file
     writer = csv.writer(f, delimiter='\t') #separate data form the same row with a tab
